3747125aa537ec58650b26881759b6a85cda6366
EDAIG KHALID <edaigkhalid@gmail.com>
2022-08-06 23:17:09 +0100

    first commit

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>From Docker</h1>
+    Hello {{ name }} welcome to web app.
+    <hr />
+    <span>with docker you can easy to word with your team</span>
+    <counter></counter>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Counter from "./Counter.vue";
+
+export default defineComponent({
+    components: {
+        Counter,
+    },
+    setup() {
+        const name = ref<string>("Bijaya");
+        return {
+            name,
+        };
+    },
+});
+</script>
